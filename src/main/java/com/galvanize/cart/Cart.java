8ff66a0b9b42feec7b283b397c580ca8d6c7c07d
RubenV-Dev <r.will16@hotmail.com>
2021-04-26 13:43:31 -0400

    test case 1

--- src/main/java/com/galvanize/cart/Cart.java
+++ src/main/java/com/galvanize/cart/Cart.java
@@ -1,4 +1,8 @@
 package com.galvanize.cart;
 
+//As a shopper
+//        I want to have a shopping cart
+//        So that I can store items until I am ready to checkout
 public class Cart {
+
 }
--- src/test/java/com/galvanize/cart/CartTest.java
+++ src/test/java/com/galvanize/cart/CartTest.java
@@ -10,5 +10,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CartTest {
 
+    @Test
+    public void whenInitializedCartIsEmpty(){
+        //Setup
+        int expected = 0;
+        Cart cart = new Cart();
+        //Execute
+        int actual = cart.getList().size();
+        //Assert
+        assertEquals(expected,actual,"cart size should be 0");
+    }
 
 }
